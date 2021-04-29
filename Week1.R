0113d25f8b202437264985cb347a09b52b6ea4f4
Ricardo Cruz <ricardocm9919@gmail.com>
2021-04-29 00:48:53 -0500

    first commit

---
+++ Week1.R
@@ -0,0 +1,8 @@
+# para convertir el tipo de los datos de un vector usamos 
+# as.numeric(), as.complex(), as.logical(), por ejemplo
+x<-c(0:6)
+as.logical(x)
+
+#para indexar en listas se comienza desde 1
+y<-list(1,2,"3")
+y[[1]]
