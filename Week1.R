dd6b9004c708bfd9ac65c078e600e95e5cc659cc
Ricardo Cruz <83147642+RichCM99@users.noreply.github.com>
2021-05-07 11:57:36 -0500

    Delete Week1.R

--- Week1.R
+++
@@ -1,8 +0,0 @@
-# para convertir el tipo de los datos de un vector usamos 
-# as.numeric(), as.complex(), as.logical(), por ejemplo
-x<-c(0:6)
-as.logical(x)
-
-#para indexar en listas se comienza desde 1
-y<-list(1,2,"3")
-y[[1]]
